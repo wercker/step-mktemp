7c2eab61ce059b4fef5dace159464d2c32ff8d65
Benno van den Berg <bennovandenberg@gmail.com>
2013-08-23 14:00:23 +0200

    Update run.sh
    Fixed typo

--- run.sh
+++ run.sh
@@ -16,7 +16,7 @@ temp_path=$($mktemp_command)
 export $WERCKER_MKTEMP_ENVVAR=$temp_path
 
 if [ "$WERCKER_MKTEMP_DIRECTORY" = "true" ] ; then
-  success "succesfully created temporary directory at $temp_path and stored it in $WERCKER_MKTEMP_ENVVAR"
+  success "successfully created temporary directory at $temp_path and stored it in $WERCKER_MKTEMP_ENVVAR"
 else
-  success "succesfully created temporary file at $temp_path and stored it in $WERCKER_MKTEMP_ENVVAR"
+  success "successfully created temporary file at $temp_path and stored it in $WERCKER_MKTEMP_ENVVAR"
 fi
